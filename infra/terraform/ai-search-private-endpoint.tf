f2f4b3652bd59227375fb052b1c46febdf2a77d7
Yass07 <i.jaskelevic@gmail.com>
2026-01-14 14:42:09 +0100

    add private endpoint + private dns zone group

---
+++ infra/terraform/ai-search-private-endpoint.tf
@@ -0,0 +1,28 @@
+############################################
+# Azure AI Search - Private Endpoint (step 3)
+# - Private Endpoint in snet-private-endpoints-dev
+# - Private DNS zone group for privatelink.search.windows.net
+############################################
+
+resource "azurerm_private_endpoint" "search" {
+  name                = "pe-search-${local.env}"
+  resource_group_name = azurerm_resource_group.core.name
+  location            = local.location
+  subnet_id           = azurerm_subnet.private_endpoints.id
+
+  private_service_connection {
+    name                           = "psc-search-${local.env}"
+    private_connection_resource_id = azurerm_search_service.search.id
+    is_manual_connection           = false
+
+
+    subresource_names = ["searchService"]
+  }
+
+  private_dns_zone_group {
+    name                 = "pdzg-search-${local.env}"
+    private_dns_zone_ids = [azurerm_private_dns_zone.core["search"].id]
+  }
+
+  tags = local.tags
+}
